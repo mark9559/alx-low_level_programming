1acef8d167a65a6312659f30f26d25dcc5ee369d
mark9559 <mwangimark57@gmail.com>
2023-02-21 12:03:01 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+
+/**
+ * main - prints all possible combinations of single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z') {
+        _putchar(letter);
+        letter++;
+    }
+    _putchar('\n');
+}
+
