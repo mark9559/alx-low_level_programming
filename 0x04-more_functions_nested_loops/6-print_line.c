c1f46085fd987c8d740f348ed1e3300deb5edbb3
mark9559 <mwangimark57@gmail.com>
2023-02-24 06:36:25 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,13 +5,14 @@
  * @n: number of dashes to print
  * Return: has no return value
  */
+
 void print_line(int n)
 {
 	int counter = 0;
 
 	while (n > 0 && counter < n)
 	{
-		_putchar('_')
+		_putchar('_');
 		counter++;
 	}
 	_putchar('\n');
