bfacf98dc1300b40ba42715281faa0e20e421918
mark9559 <mwangimark57@gmail.com>
2023-02-27 10:56:40 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+0. void reset_to_98(int *n);
+1  void swap_int(int *a, int *b);
+2  int _strlen(char *s);
+3  void _puts(char *str);
+4  void print_rev(char *s);
+5  void rev_string(char *s);
+6  void puts2(char *str);
+7  void puts_half(char *str);
+8  void print_array(int *a, int n);
+9  char *_strcpy(char *dest, char *src);
+10 int _atoi(char *s);
+
